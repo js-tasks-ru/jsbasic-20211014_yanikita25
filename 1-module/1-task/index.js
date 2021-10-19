110be52e2e4cb547d30eb37186da553c9d13f320
nikita-yarmola <ya.nikita.yarmola@gmail.com>
2021-10-19 23:32:07 +0300

    1-module 1-task completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,9 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n !== 1 && n !== 0) {
+    for (let i = 1; i < n; i++) {
+      result *= (i + 1);
+    }
+  }
+  return result;
 }
