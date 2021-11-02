36bbb1fd8dc089a2e8bc6f84951ed1698ec3c2d0
nikita-yarmola <ya.nikita.yarmola@gmail.com>
2021-11-02 23:55:50 +0300

    3-module 5-task completed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  let splitted = str.split(' ');
+  let nmbrs = [];
+  let result = {
+    min: 0,
+    max: 0,
+  }
+  splitted.forEach(function (item) {
+    if (!isNaN(item)) {
+      nmbrs.push(item)
+    }
+  })
+  nmbrs.sort((a, b) => a - b);
+  result.max = +nmbrs[nmbrs.length - 1];
+  result.min = +nmbrs[0];
+  return result;
 }
