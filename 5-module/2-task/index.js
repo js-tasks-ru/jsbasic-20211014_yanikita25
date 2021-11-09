4f1831c25eba7f4f5c447701fb8b5825a8bfb2a8
nikita-yarmola <ya.nikita.yarmola@gmail.com>
2021-11-09 23:15:45 +0300

    5-module 2-task completed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+    button.onclick = function () {
+        text.hasAttribute('hidden') ?
+            text.removeAttribute('hidden') : text.setAttribute('hidden', '');
+    }
 }
