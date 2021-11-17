4133ed8b19f236e4afaf0b15f8e194001931880e
nikita-yarmola <ya.nikita.yarmola@gmail.com>
2021-11-17 21:42:11 +0300

    6-module 1-task completed

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,40 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    this.addTableRow();
+  }
+  addTableRow() {
+    this.tBody = this.elem.querySelector('tbody');
+    for (let row of this.rows) {
+      let tableRow = document.createElement('tr');
+      tableRow.innerHTML = `<td>${row.name}</td>
+      <td>${row.age}</td>
+      <td>${row.salary}</td>
+      <td>${row.city}</td>
+      <td><button>X</button></td>`;
+      this.tBody.append(tableRow);
+    }
+    this.tBody.onclick = this.removeRow;
+  }
+  removeRow(event) {
+    if (event.target.tagName == 'BUTTON') {
+      event.target.parentNode.parentNode.remove();
+    }
+  }
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>`;
   }
 }
